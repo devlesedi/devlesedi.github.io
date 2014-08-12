6bf7ef6e5ea151fd9e9508fe0243c9787a88c32d
Lesedi Ramahobo <devlesedi@gmail.com>
2014-08-12 18:17:59 +0200

    v1.10

---
+++ assets/js/main.js
@@ -0,0 +1,82 @@
+var initNav = function() {
+	$('.tabs li').click(function(e) {
+		e.preventDefault();
+		var $self = $(this);
+		var targetId = $(this).attr('id');
+		var activeTab = $('.tabs li.active').attr('id');
+		$('.content_wrapper').find('#' + activeTab).css({display: 'none'});
+		$('.content_wrapper').find('#' + targetId).slideToggle(300, function() {
+			$('.tabs li.active').toggleClass('active');
+			$self.toggleClass('active');
+		});
+	});
+};
+
+var parseAPPKEY = "XuMntF7TTSxdRJyoOUYQ2M58kpOWKb7wqdUpWXwC",
+	parseJSID = "gwrjv9OcEIU2YEROTfAOoiEDwJTnxpf7UI8f7BPB",
+	$commentForm = $('#commentForm');
+
+Parse.initialize(parseAPPKEY, parseJSID);
+var PSDMailObject = Parse.Object.extend("PSDMailObject");
+
+function onCommFormSubmit(evt) {
+	evt.preventDefault();
+	var data = {};
+	data.name = $('input[name="name"]').val();
+	data.email = $('input[name="email"]').val();
+	data.comment = $('textarea[name="comment"]').val();
+
+	var mail = new PSDMailObject();
+	mail.save(data, {
+		success:function(res) {
+			console.log("Success" + res);
+
+			//Alerts are lame - but quick and easy
+			alert("Thanks for filling the form!");
+		},
+		error:function(e) {
+			console.dir(e);
+		}
+	});
+
+};
+
+$(document).ready(function() {
+	initNav();
+
+	$commentForm.submit(function(evt) {
+		onCommFormSubmit(evt);
+	});
+
+	function Validator()
+	{
+	    "use strict";
+	}
+
+	Validator.prototype.checkName = function(name)
+	{
+	    "use strict";
+	    return (/[^a-z]/i.test(name) === false);
+	};
+	$('.required').each(function() {
+		$(this).on('blur', function() {
+			var _this = this;
+			var $self = $(this);
+	        var validator = new Validator();
+	        $('.validator').html("");
+	        if (validator.checkName(_this.value) === true && _this.value !== '') {
+	        	if ($self.hasClass('input-danger')) {
+	        		$self.removeClass('input-danger');
+	        	};
+	            $self.addClass("input-success");
+	        }
+	        else {
+	        	if ($self.hasClass('input-success')) {
+	        		$self.removeClass('input-success');
+	        	};
+	            $self.addClass("input-danger");
+	            $('#validator-' + $self.attr('name')).html("Required");
+	        }
+		});
+	});
+});
